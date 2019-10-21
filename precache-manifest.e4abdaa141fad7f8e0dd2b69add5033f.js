f9546e4c2b0ff534744ee9cc529552ed4d90fb6b
marco741 <marco741147@gmail.com>
2019-10-21 19:02:43 +0200

    Updates

---
+++ precache-manifest.e4abdaa141fad7f8e0dd2b69add5033f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72333959b09e21a4b8f3c8ad8d8d704",
+    "url": "/ecomm_react/index.html"
+  },
+  {
+    "revision": "a9e1cf9039297f2de9fc",
+    "url": "/ecomm_react/static/css/2.55521911.chunk.css"
+  },
+  {
+    "revision": "e3a03a4dd2c84d3f844b",
+    "url": "/ecomm_react/static/css/main.26c5c745.chunk.css"
+  },
+  {
+    "revision": "a9e1cf9039297f2de9fc",
+    "url": "/ecomm_react/static/js/2.4a983a43.chunk.js"
+  },
+  {
+    "revision": "e3a03a4dd2c84d3f844b",
+    "url": "/ecomm_react/static/js/main.b0a2d122.chunk.js"
+  },
+  {
+    "revision": "745669bdefb7190d5f88",
+    "url": "/ecomm_react/static/js/runtime-main.10b97693.js"
+  }
+]);
